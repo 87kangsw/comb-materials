d306dc8dc26d02c626dca4d1a33b1a4e122333ea
Kanz <87kangsw@gmail.com>
2022-01-16 23:29:31 +0900

    Ch7 Study

--- 07-sequence-operators/projects/Starter.playground/Contents.swift
+++ 07-sequence-operators/projects/Starter.playground/Contents.swift
@@ -3,7 +3,155 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "min") {
+    let publisher = [1, -50, 246, 0].publisher
+    
+    publisher
+        .print("publisher")
+        .min()
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "min non-Comparable") {
+    let publisher = [
+        "12345",
+        "ab",
+        "hello world"
+    ]
+        .map { Data($0.utf8) }
+        .publisher
+    
+    publisher
+        .print("publisher")
+        .min(by: { $0.count < $1.count })
+        .sink { data in
+            let string = String(data: data, encoding: .utf8)
+            print("Smallest data is \(string), \(data.count) bytes")
+        }
+        .store(in: &subscriptions)
+}
+
+example(of: "max") {
+    let publisher = ["A", "F", "Z", "E"].publisher
+    
+    publisher
+        .print("publisher")
+        .max()
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "first") {
+    let publisher = ["A", "B", "C"].publisher
+    
+    publisher
+        .print()
+        .first()
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "first(where:)") {
+    let publisher = ["J", "O", "H", "N"].publisher
+    
+    publisher
+        .print()
+        .first(where: { "Hello world".contains($0) })
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "last") {
+    let publisher = ["A", "B", "C"].publisher
+    
+    publisher
+        .print()
+        .last()
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "output(at:)") {
+    let publisher = ["A", "B", "C"].publisher
+    
+    publisher
+        .print()
+        .output(at: 1)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "output(in:)") {
+    let publisher = ["A", "B", "C", "D", "E"].publisher
+    
+    publisher
+        .output(in: 1...3)
+        .sink(receiveCompletion: { print($0) }, receiveValue: { print("Value in range: \($0)")})
+        .store(in: &subscriptions)
+}
+
+example(of: "count") {
+    let publisher = ["A", "B", "C"].publisher
+    
+    publisher
+        .print()
+        .count()
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "contains") {
+    let publisher = ["A", "B", "C", "D", "E"].publisher
+    let letter = "F"
+    
+    publisher
+        .contains(letter)
+        .sink(receiveValue: { contains in print(contains ? "emitted :\(letter)" : "never emitted: \(letter)") })
+        .store(in: &subscriptions)
+}
+
+example(of: "contains(where:)") {
+    
+    struct Person {
+        let id: Int
+        let name: String
+    }
+    
+    let people = [
+        (123, "Hahaha"),
+        (456, "ooops"),
+        (214, "hhohoho")
+    ]
+        .map(Person.init)
+        .publisher
+    
+    people
+        .contains(where: { $0.id == 123 })
+        .sink(receiveValue: { contains in print(contains ? "matches" : "couldn't find") })
+        .store(in: &subscriptions)
+}
+
+example(of: "allSatisfy") {
+    let publisher = stride(from: 0, to: 5, by: 1).publisher
+    
+    publisher
+        .print()
+        .allSatisfy { $0 % 2 == 0 }
+        .sink(receiveValue: { allEven in print(allEven ? "all even" : "odd") })
+        .store(in: &subscriptions)
+}
+
+example(of: "reduce") {
+    let publisher = ["Hel", "lo", " " , "Wor", "ld", "!"].publisher
+    
+    publisher
+        .print()
+        .reduce("", { accumulator, value in accumulator + value })
+        .sink(receiveValue: { print("reduced:: \($0)") })
+        .store(in: &subscriptions)
+}
+
 
 /// Copyright (c) 2021 Razeware LLC
 ///
