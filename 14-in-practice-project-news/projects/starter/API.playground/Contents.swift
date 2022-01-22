1e1f5075def6c849eafff9bcb448017f03f8b6fd
Kanz <87kangsw@gmail.com>
2022-01-22 23:00:32 +0900

    Ch 14 study

--- 14-in-practice-project-news/projects/starter/API.playground/Contents.swift
+++ 14-in-practice-project-news/projects/starter/API.playground/Contents.swift
@@ -32,18 +32,72 @@ struct API {
       }
     }
   }
-
-  /// Maximum number of stories to fetch (reduce for lower API strain during development).
-  var maxStories = 10
-
-  /// A shared JSON decoder to use in calls.
-  private let decoder = JSONDecoder()
+    
+    /// Maximum number of stories to fetch (reduce for lower API strain during development).
+    var maxStories = 10
+    
+    private let apiQueue = DispatchQueue(label: "API", qos: .default, attributes: .concurrent)
+    
+    /// A shared JSON decoder to use in calls.
+    private let decoder = JSONDecoder()
+    
+    func story(id: Int) -> AnyPublisher<Story, Error> {
+        return URLSession.shared
+            .dataTaskPublisher(for: EndPoint.story(id).url)
+            .receive(on: apiQueue)
+            .map(\.data)
+            .decode(type: Story.self, decoder: decoder)
+            .catch { _ in Empty<Story, Error>() }
+            .eraseToAnyPublisher()
+    }
   
-  // <#Add your API code here#>
-  
+    
+    func mergedStories(ids storyIDs: [Int]) -> AnyPublisher<Story, Error> {
+        let storyIDs = Array(storyIDs.prefix(maxStories))
+        
+        precondition(!storyIDs.isEmpty)
+        
+        let initialPublisher = story(id: storyIDs[0])
+        let remainder = Array(storyIDs.dropFirst())
+        
+        return remainder.reduce(initialPublisher) { combined, id in
+            return combined
+                .merge(with: story(id: id))
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    func stories() -> AnyPublisher<[Story], Error> {
+        URLSession.shared
+            .dataTaskPublisher(for: EndPoint.stories.url)
+            .map(\.data)
+            .decode(type: [Int].self, decoder: decoder)
+            .mapError { error -> API.Error in
+                switch error {
+                case is URLError:
+                    return Error.addressUnreachable(EndPoint.stories.url)
+                default:
+                    return Error.invalidResponse
+                }
+            }
+            .filter { $0.isEmpty == false }
+            .flatMap { storyIDs in
+                return self.mergedStories(ids: storyIDs)
+            }
+            .scan([]) { stories, story -> [Story] in
+                return stories + [story]
+            }
+            .map { $0.sorted() }
+            .eraseToAnyPublisher()
+    }
 }
 
-// <#Call the API here#>
+let api = API()
+var subscriptions = [AnyCancellable]()
+
+api.stories()
+    .sink(receiveCompletion: { print($0) }, receiveValue: { print($0) })
+    .store(in: &subscriptions)
 
 
 // Run indefinitely.
